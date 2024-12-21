cac8ec8a9b2573f790ebf70ea2cad5d3e3ddd928
Marcellius <marcelliusfelixmatius@gmail.com>
2024-12-21 13:16:31 +0700

    perubahan 2

--- index.js
+++ index.js
@@ -26,14 +26,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Variable to store the latest message
 let latestMessage = 'No messages yet';
 let parsedData = { co2: 0, nh3: 0, nox: 0 };
+const sensorData = {
+  sensor1: { co2: 0, nh3: 0, nox: 0 },
+  sensor2: { co2: 0, nh3: 0, nox: 0 },
+  sensor3: { co2: 0, nh3: 0, nox: 0 },
+};
 
 // MQTT Client Event Handlers
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
-  client.subscribe('MQ135/sensor1', (err) => {
-    if (!err) {
-      console.log('Subscribed to topic: MQ135/sensor1');
-    }
+  // client.subscribe('MQ135/sensor1', (err) => {
+  //   if (!err) {
+  //     console.log('Subscribed to topic: MQ135/sensor1');
+  //   }
+  // });
+  // Subscribe to both sensor topics
+  ['MQ135/sensor1', 'MQ135/sensor2','MQ135/sensor3'].forEach((topic) => {
+    client.subscribe(topic, (err) => {
+      if (!err) {
+        console.log(`Subscribed to topic: ${topic}`);
+      }
+    });
   });
 });
 
@@ -50,18 +63,25 @@ client.on('message', (topic, message) => {
       nh3: parseFloat(match[2]),
       nox: parseFloat(match[3]),
     };
+
+    // Update the respective sensor data
+    if (topic === 'MQ135/sensor1') {
+      sensorData.sensor1 = parsedData;
+    } else if (topic === 'MQ135/sensor2') {
+      sensorData.sensor2 = parsedData;
+    } else if (topic === 'MQ135/sensor3'){
+      sensorData.sensor3 = parsedData;
+    }
   }
 });
 
 // Route ke halaman dashboard
 app.get('/', (req, res) => {
-  res.render('dashboard', { 
-    message: latestMessage,
-    data: parsedData
-  });
+  res.render('dashboard', { data: sensorData });
 });
 
 app.get('/api/data', (req, res) => {
+  res.set('Cache-Control', 'no-store'); // Prevent caching
   res.json(parsedData);
 });
 
